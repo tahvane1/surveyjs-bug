4730a69da72fa307154ac5d4434ce6390d3a0c93
Erkka Tahvanainen <erkka.tahvanainen@confidently.fi>
2025-08-13 12:12:01 +0300

    updates

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,35 @@
+
+'use client'
+
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+
+import { useCallback } from 'react';
+import 'survey-core/survey-core.css';
+import { Model } from 'survey-core'
+import { Survey } from 'survey-react-ui'
+
+// const SURVEY_ID = 1;
+
+const surveyJson = {
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "dropdown",
+          "name": "question1",
+          "choices": [
+            "Item 1",
+            "Item 2",
+            "Item 3"
+          ]
+        }
+      ]
+    }
+  ]
+};
+;
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +39,6 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  const model = new Model(surveyJson)
+  return <Survey model={model} />
 }
